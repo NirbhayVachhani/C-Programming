9006d78d00abceb2b5e2f6dd10187224e3fc9a0d
NirbhayVachhani <76275418+NirbhayVachhani@users.noreply.github.com>
2021-07-09 20:41:55 +0530

    Update Swap_Value.c

--- Swap_Value.c
+++ Swap_Value.c
@@ -7,6 +7,11 @@ int main()
     c=a;
     a=b;
     b=c;
-    printf("After swapping value of a = %d\n b =%d",a,b);
+    printf("After swapping value of a = %d\n b =%d\n",a,b);
+    
+    a = a^b;
+    b = a^b;
+    a=  a^b;
+    printf("\n\nAfter swapping value of a and b : %d,%d\n\n",a,b);
     return 0;
 }
